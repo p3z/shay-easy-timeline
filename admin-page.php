a9f12cbb4772106789c997b0dc98d09574702d31
Shay <chenille.pottle@yahoo.co.uk>
2023-12-24 17:57:02 +0000

    Some progress

--- admin-page.php
+++ admin-page.php
@@ -1,63 +1,137 @@
-<?php
-
-function get_timelines() {
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'pws_timelines';        
-        $timelines = $wpdb->get_var("SELECT * FROM $table_name ORDER BY id DESC");        
-        $query = "SELECT * FROM $table_name";
-        $timelines = $wpdb->get_results($query, ARRAY_A);
-
-        //echo "<br><br><br>";
-
-        // Now $results contains an array of all records from the table
-        foreach ($timelines as $timeline) {
-                echo "<tr>";
-                        echo "<td>" . $timeline['id'] . "</td>";
-                        echo "<td>" . $timeline['timeline_title'] . "</td>";
-                        echo "<td>" . "...actions..." . "</td>";
-                echo "</tr>";
-        // Access individual fields like $result['id'], $result['timeline_title'], etc.
-        //echo $timeline['id'] . ': ' . $timeline['timeline_title'] . '<br>';
-        }
-
-        // "display_date": "",
-// "safe_date": "",
-// "title": "",
-// "description": "",
-// "order": 0
-    
-        return $data_value;
-}
-?>
+<?php session_start(); ?>
 
 <div class="wrap">
-        <h1>Timelines</h1>
-        <table class="pws-timeline-table">
-                <tr>
-                        <th>id</th>
-                        <th>title</th>
-                        <th>actions</th>
-                </tr>
-                <?php get_timelines(); ?>
-        </table>
+
+<?php
+
+        if (isset($_GET['timeline'])) {
+                // viewing single
+                echo "<a href=''>View all timelines</a>";
+                echo "<h1>Viewing: " . $_GET['timeline'];
+
+                function get_single_timeline($timeline_id) {
+                        global $wpdb;
+                        $table_name = $wpdb->prefix . 'pws_timelines';                        
+                        $query = $wpdb->prepare("SELECT * FROM $table_name WHERE id = %d", $timeline_id);
+
+                        // Get the result
+                        $result = $wpdb->get_row($query);
+
+                        // Check if a row was found
+                        if ($result) {
+                                echo "-------------------------------";
+                                print_r($result);
+                                echo "-------------------------------";
+                                // // Access the data
+                                // $id = $result->id;
+                                // $timeline_title = $result->timeline_title;
+
+                                // // Do something with the data
+                                // echo "ID: $id, Title: $timeline_title";
+                        } else {
+                                return null;
+                        }
+                    
+                        // Retrieve the data from the custom table
+                        $timeline = $wpdb->get_var("SELECT data_value FROM $table_name ORDER BY id DESC LIMIT 1");
+                    
+                    
+                        return $timeline;
+                } // end get_single_timeline
+
+                $timeline = get_single_timeline($timeline_id);
+
+                // "display_date": "",
+                // "safe_date": "",
+                // "title": "",
+                // "description": "",
+                // "order": 0
+
+
+        ?>
+
+                <form class="pws-edit-timeline-form" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
+                        <?php wp_nonce_field('handle_timeline_form_nonce', '_wpnonce'); ?>
+
+                        <input type="hidden" name="action" value="handle_timeline_form_hook">
+                        
+                        <label for="timeline_title">Timeline name:</label><br />
+                        <input id="timeline_title" type="text" name="timeline_title" value="<?php echo $timeline->title ?? ''; ?>"><br />
+
+                        <button class="pws-btn">
+                                Update
+                        </button>
+
+                </form>
+
+
+        <?php
+                
+        } else {
+                // viewing all
+
+                function get_timelines() {
+                        global $wpdb;
+                        $table_name = $wpdb->prefix . 'pws_timelines';        
+                        $timelines = $wpdb->get_var("SELECT * FROM $table_name ORDER BY id DESC");        
+                        $query = "SELECT * FROM $table_name";
+                        $timelines = $wpdb->get_results($query, ARRAY_A);
         
-        <h2>Create new timeline</h2>
-        <button class="js-toggle-create-form pws-btn">
-                Create new timeline
-        </button>
+                        // Now $results contains an array of all records from the table
+                        foreach ($timelines as $timeline) {
+        
+                                $single_route = add_query_arg('timeline', $timeline['id'], wp_get_referer());
+        
+        
+                                echo "<tr>";
+                                        echo "<td>" . $timeline['id'] . "</td>";
+                                        echo "<td>" . $timeline['timeline_title'] . "</td>";
+                                        echo "<td><a class='timeline-btn' href='$single_route'>Details</a></td>";
+                                echo "</tr>";
+                        // Access individual fields like $result['id'], $result['timeline_title'], etc.
+                        //echo $timeline['id'] . ': ' . $timeline['timeline_title'] . '<br>';
+                        }
+        
+             
+                
+                } 
+             
 
-        <form class="pws-create-timeline-form hide" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
-                <?php wp_nonce_field('handle_timeline_form_nonce', '_wpnonce'); ?>
-
-                <input type="hidden" name="action" value="handle_timeline_form_hook">
-                      
-                <label for="timeline_title">Timeline name:</label><br />
-                <input id="timeline_title" type="text" name="timeline_title"><br />
-
-                <button class="pws-btn">
-                        Create
+                if (isset($_SESSION['success'])) {
+                        $message = sanitize_text_field($_SESSION['success']);
+                        echo '<div class="success-message">' . esc_html($message) . '</div>';
+                        unset($_SESSION['success']); // Clear the session variable after displaying the message
+                }
+           
+?>                
+                <button class="js-toggle-create-form pws-btn">
+                        Create new timeline
                 </button>
 
-        </form>
+                <form class="pws-create-timeline-form hide" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
+                        <?php wp_nonce_field('handle_timeline_form_nonce', '_wpnonce'); ?>
+
+                        <input type="hidden" name="action" value="handle_timeline_form_hook">
+                        
+                        <label for="timeline_title">Timeline name:</label><br />
+                        <input id="timeline_title" type="text" name="timeline_title"><br />
+
+                        <button class="pws-btn">
+                                Create
+                        </button>
+
+                </form>
+
+                <h1>Existing timelines</h1>
+                <table class="pws-timeline-table">
+                        <tr>
+                                <th>id</th>
+                                <th>title</th>
+                                <th>actions</th>
+                        </tr>
+                        <?php get_timelines(); ?>
+                </table>
+
+<?php   } // end if/else ?>
 
 </div>
--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /*
     Plugin Name: Shay Easy Timeline
     Description: Save me from figuring this out manually time and again
-    Version: 1.0.13
+    Version: 1.0.19
     Author: Shay Pottle
     Author URI: https://shaypottle.com
     License: MIT
@@ -12,6 +12,9 @@
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
+
+session_start();
+
 
 class ShayEasyTimeline {
     
@@ -41,11 +44,11 @@ class ShayEasyTimeline {
         add_menu_page(
             'Shay Easy Timeline', // Page title
             'Shay Easy Timeline', // Menu title
-            'manage_options',               // Capability
+            'manage_options', // Capability
             'shay-easy-timeline', // Menu slug
             array($this, 'display_admin_page'), // Callback function to display page content
             'dashicons-clock',
-            99                              // Menu position
+            99 // Menu position
         );
     }
     
@@ -68,7 +71,6 @@ class ShayEasyTimeline {
     public function handle_timeline_form() {
         global $wpdb;
 
-
         if (!isset($_POST['_wpnonce']) || !wp_verify_nonce($_POST['_wpnonce'], 'handle_timeline_form_nonce')) {
             echo 'Form failed!<br>';
             wp_die('Security check failed');
@@ -81,19 +83,13 @@ class ShayEasyTimeline {
             $timeline_title = sanitize_text_field($_POST['timeline_title']);
             $wpdb->insert($table_name, array('timeline_title' => $timeline_title), array('%s'));
         }
-        // Handle form data
-        // $form_data = isset($_POST['form_data']) ? sanitize_text_field($_POST['form_data']) : '';
-        // // "display_date": "",
-        // // "safe_date": "",
-        // // "title": "",
-        // // "description": "",
-        // // "order": 0
-
-        // // Process form data (save to the database, send emails, etc.)
         
-        $redirect_url = add_query_arg('shay_timeline_form_submitted', 'true', wp_get_referer());
-        wp_safe_redirect($redirect_url);
+
+        $_SESSION['success'] = 'Form submitted successfully';
+        
+        wp_safe_redirect(wp_get_referer());
         exit;
+    
     }
 
     public function create_timeline_table() {
@@ -113,15 +109,40 @@ class ShayEasyTimeline {
         dbDelta($sql);
     }
 
-    public function drop_table() {
+    public function create_timeline_single_table() {
         global $wpdb;
-        $table_name = $wpdb->prefix . 'pws_timelines';
-        $wpdb->query("DROP TABLE IF EXISTS $table_name");
+        
+        $table_name = $wpdb->prefix . 'pws_timelines_single';    
+        $charset_collate = $wpdb->get_charset_collate();
+    
+        $sql = "CREATE TABLE $table_name (
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            timeline_id int NOT NULL,
+            display_date varchar(255),
+            safe_date varchar(255),
+            description varchar(255),
+            order int NOT NULL DEFAULT 0,
+            PRIMARY KEY  (id)
+        ) $charset_collate;";
+    
+    
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        dbDelta($sql);
+    }
+
+    public function drop_tables() {
+        global $wpdb;
+
+        $pws_timelines = $wpdb->prefix . 'pws_timelines';
+        $wpdb->query("DROP TABLE IF EXISTS $pws_timelines");
+
+        $pws_timelines_single = $wpdb->prefix . 'pws_timelines_single';        
+        $wpdb->query("DROP TABLE IF EXISTS $pws_timelines_single");
+
     }
     
 }
 
-// Instantiate your plugin class.
 $your_plugin_instance = new ShayEasyTimeline();
 
 
@@ -129,10 +150,11 @@ $your_plugin_instance = new ShayEasyTimeline();
 
 register_activation_hook(__FILE__, array('ShayEasyTimeline', 'activate'));
 register_activation_hook(__FILE__, array('ShayEasyTimeline', 'create_timeline_table'));
+register_activation_hook(__FILE__, array('ShayEasyTimeline', 'create_timeline_single_table'));
 
 
 
 register_deactivation_hook(__FILE__, array('ShayEasyTimeline', 'deactivate'));
 register_uninstall_hook(__FILE__, array('ShayEasyTimeline', 'uninstall'));
-register_uninstall_hook(__FILE__, array('ShayEasyTimeline', 'drop_table'));
+register_uninstall_hook(__FILE__, array('ShayEasyTimeline', 'drop_tables'));
 
